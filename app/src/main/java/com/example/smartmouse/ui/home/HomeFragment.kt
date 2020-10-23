2fc597e570190c0bd18191ca9a8e5be80b37455e
yoshiaki-shiokawa <69422147+yoshiaki-shiokawa@users.noreply.github.com>
2020-10-23 16:41:33 +0100

    Changed fragment_home.xml for mouse Add button_state.xml, button_shape1.xml, button_shape2.xml

--- app/src/main/java/com/example/smartmouse/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/smartmouse/ui/home/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -22,10 +24,10 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val AirMouseButton: Button = root.findViewById(R.id.button_airmouse)
+        val MouseState: ToggleButton = root.findViewById(R.id.toggle_mouse)
+        AirMouseButton.setOnClickListener { }
+        MouseState.setOnClickListener {  }
         return root
     }
 }
